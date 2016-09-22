aafac9918781817674ea76713c00bb4205d2fd53
DAM <alvarosanchezdam@gmail.com>
2016-09-22 20:36:26 +0200

    Actualizacion

--- src/HashMap/SeguridadSocial.java
+++ src/HashMap/SeguridadSocial.java
@@ -16,29 +16,42 @@ public class SeguridadSocial {
 
     // Debes comprobar que no se introduzcan dos personas con el mismo DNI/Número Seguridad Social
     public void altaPersona(Persona persona) {
-        if(personaMapDni.containsValue(persona.getDni())==false||personaMapNumSS.containsValue(persona.getNumSeguridadSocial())==false){
+        //Muestra todos los valores(Personas) de un hash, stream los muestra en flujo, anyMatch para cuando ocurra lo indicado en este caso que el dni de la persona del hash sea igual al de la persona que se introduce
+        boolean repetida = personaMapDni.values().stream().anyMatch(persona1 -> persona1.getDni().equals(persona.getDni()));
+        //for(Persona p : personaMapDni.values()){
+        //}
+        if(repetida==false){
             personaMapDni.put(persona.getDni(), persona);
             personaMapNumSS.put(persona.getNumSeguridadSocial(), persona);
-        }
-        else{
-            System.out.println("Dni o Numero de la seguridad social repetidos");
         }
     }
 
     public void bajaPersona(String dni) {
+        for(Persona p : personaMapDni.values()){
+            if(p.getDni().equals(dni)){
+                personaMapNumSS.remove(p.getNumSeguridadSocial());
+            }
+        }
+        personaMapDni.remove(dni);
 
     }
 
     public Persona obtenerPersonaPorDNI(String dni) {
-
+        return personaMapDni.get(dni);
     }
 
     public Persona obtenerPersonaPorNumSS(String numSS) {
-
+        return personaMapDni.get(numSS);
     }
 
-    public List<Persona> obtenerPersonasRangoSalarial(double min, double max){
-
+    public Map<String, Persona> obtenerPersonasRangoSalarial(double min, double max){
+        Map<String, Persona> personasSalarioDni=new HashMap<>();
+        for(Persona p : personaMapDni.values()){
+            if(p.getSalario()>=min&&p.getSalario()<=max){
+                personasSalarioDni.put(p.getDni(), p);
+            }
+        }
+        return personasSalarioDni;
     }
 
     public List<Persona> obtenerPersonasMayoresQue(int edad){
@@ -52,7 +65,7 @@ public class SeguridadSocial {
     @Override
     public String toString() {
         return "SeguridadSocial{" +
-                "personasList=" + personasList +
+                "personasList=" +
                 '}';
     }
 }
